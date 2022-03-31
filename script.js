4cd445023db39092d2a416b22f3877c2333fddf4
Viktor <33.1_tm@mail.ru>
2022-04-01 01:00:11 +0600

    начало

--- script.js
+++ script.js
@@ -1,82 +1,27 @@
-const phoneNumberData = {
-    phoneNumber: "",
-};
-
-let input = document.querySelectorAll('input[type="tel"]');
-const telNo = document.querySelector(".telNo");
-const cross = document.querySelector(".cross");
-const sendNo = document.querySelector(".sendNo");
-const error = document.querySelector(".error");
-
-let im = new Inputmask({mask:'+7 (999) 999-99-99', autoUnmask: true,
-placeholder: "+7 (   )    -  -  "}, ).mask(input);
+const button = document.querySelector('.button'),
+      popUpWindow = document.querySelector('.popUpWindow');
 
 
-function bindInputEventToElement() {
-    telNo.addEventListener('input', (event) => {
-        isInputNotEmpty() ? showCross() : hideCross()
-        writeValueFromInputToPhoneNumberData();
-    })
+button.addEventListener('click', () => {
+    showPopUpWindow();
+    showPopUpSetTimeOut();
+})
+
+popUpWindow.addEventListener('click', (e) => {
+    console.dir(e.target);
+})
+
+
+function showPopUpSetTimeOut() {
+    setTimeout(() => {
+        popUpWindow.innerHTML = 'Загружено';
+    }, 1000)
 }
 
-function writeValueFromInputToPhoneNumberData() {
-    phoneNumberData.phoneNumber = telNo.value
+function showPopUpWindow() {
+    popUpWindow.style.display = "flex";
 }
 
-function bindEventClickToCross() {
-    cross.addEventListener('click', (event) => {
-        clearNumber();
-        hideCross();
-        hideMessageIfInputEmpty();
-    });
+function hidePopUpWindow() {
+    popUpWindow.style.display = "none";
 }
-
-function clearNumber() {
-    telNo.value = "";
-}
-
-function showCross() {
-    cross.style.display = "flex";
-}
-
-function hideCross() {
-    cross.style.display = "none";
-}
-
-function isInputNotEmpty() {
-    return telNo.inputmask.unmaskedvalue().length;
-}
-
-function bindEventClckToSendButton() {
-    sendNo.addEventListener('click', () => {
-        isPhoneNumberNotFull() ?   showMessageIfInputEmpty() :  sendPhoneNumberData();
-    });
-}
-
-function bindEventToHideErrorMessage() {
-    telNo.addEventListener('input', (event) => {
-        hideMessageIfInputEmpty();
-    })
-}
-
-function isPhoneNumberNotFull() {
-    return telNo.value.length < 10 || telNo.value.length == ""
-}
-
-
-function sendPhoneNumberData() {
-    console.log(phoneNumberData);
-}
-
-function showMessageIfInputEmpty() {
-    error.style.display = "block";
-}
-
-function hideMessageIfInputEmpty() {
-    error.style.display = "none";
-}
-
-bindEventToHideErrorMessage();
-bindEventClickToCross();
-bindEventClckToSendButton();
-bindInputEventToElement();
