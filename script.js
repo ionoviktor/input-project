12791f9ea4ca9b89e2a68206951c3cf44175d8ef
Viktor <33.1_tm@mail.ru>
2022-05-04 21:36:23 +0600

    На проверку

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+'use strict';
+
 const button = document.querySelector('.button'),
       popUpWindow = document.querySelector('.popUpWindow');
 
 
-button.addEventListener('click', () => {
-    showPopUpWindow();
-    showPopUpSetTimeOut();
-})
 
-popUpWindow.addEventListener('click', (e) => {
-    console.dir(e.target);
-})
+function showInfoInWindowOnClick() {
+    button.addEventListener('click', () => {
+        setTimeout(getDataFromServer, 1000);
+        showPopUpWindow();
+    })
+}
+
+async function getDataFromServer() {
+    let response = await fetch('https://dummyjson.com/posts')
+        .then(res => res.json())
+
+    const posts = response.posts;
 
 
-function showPopUpSetTimeOut() {
-    setTimeout(() => {
-        popUpWindow.innerHTML = 'Загружено';
-    }, 1000)
+    if(response.ok) {
+        popUpWindow.innerHTML = posts.slice(0,5).map((post) => {
+            return `
+               Id: ${post.id} <br>
+               Tittle: ${post.title} <br>
+               Body: ${post.body}<br><br>`
+        })
+    } else {
+        popUpWindow.innerHTML = 'Произошла ошибка';
+    }
+
 }
 
 function showPopUpWindow() {
@@ -25,3 +39,5 @@ function showPopUpWindow() {
 function hidePopUpWindow() {
     popUpWindow.style.display = "none";
 }
+
+showInfoInWindowOnClick();
