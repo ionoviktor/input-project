4389300be7c34ea6453b81f76358376839e22a5c
Дмитрий <Dmitriy.Chernomazov@kaspi.kz>
2022-05-04 22:25:14 +0600

    Внес корректировки

--- script.js
+++ script.js
@@ -13,20 +13,32 @@ function showInfoInWindowOnClick() {
 }
 
 async function getDataFromServer() {
-    let response = await fetch('https://dummyjson.com/posts')
-        .then(res => res.json())
+    try {
+        let responseFromServer = await fetch('https://dummyjson.com/posts')
 
-    const posts = response.posts;
+        let parsedResponse = await responseFromServer.json();
+    
+    
+        const posts = parsedResponse.posts;
 
+        const ul = document.createElement('ul');
 
-    if(response.ok) {
-        popUpWindow.innerHTML = posts.slice(0,5).map((post) => {
-            return `
-               Id: ${post.id} <br>
-               Tittle: ${post.title} <br>
-               Body: ${post.body}<br><br>`
+        posts.slice(0,5).forEach((post) => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+            Id: ${post.id} <br>
+            Tittle: ${post.title} <br>
+            Body: ${post.body}<br><br>`
+
+            ul.append(li)
         })
-    } else {
+
+
+        popUpWindow.append(ul)
+        
+    
+    } catch (err) {
+        console.debug(err);
         popUpWindow.innerHTML = 'Произошла ошибка';
     }
 
